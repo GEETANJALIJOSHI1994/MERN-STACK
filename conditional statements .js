b214c2dd9753bc376278d20ea8ce4cfbd2f01de6
Geetanjali Joshi <joshigeetanjali369@gmail.com>
2025-08-17 22:15:31 +0530

    mern stack

---
+++ conditional statements .js
@@ -0,0 +1,33 @@
+// let size = "xl";
+// if(size=="xl"){
+//     console.log("prize is Rs.250");
+// }
+// else if(size=="l"){
+//      console.log("prize is Rs.200");
+
+// }
+// else if(size=="m"){
+//     console.log("prize is Rs.100");
+// }
+// else if(size=="s"){
+//     console.log("price is Rs.50");
+// }
+// else{
+//     console.log("not diffine");
+// }
+
+// nested if-else 
+
+    let marks = 34;
+    if(marks>=30){
+        console.log("pass");
+        if(marks>=80){
+            console.log("grade A+");
+        }
+        else{
+            console.log("grade A");
+        }
+    }
+    else{
+        console.log("fail");
+    }
---
+++ template.js
@@ -0,0 +1,21 @@
+// let a = 23;
+// let b = 34;
+// let output = `total number of ${a + b}`;
+// console.log(output);
+
+
+// operaters
+
+// let a = 12;
+// let b = 23;
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+// console.log(a**b);
+// console.log(++a);
+// console.log(a++);
+
+// comparison operaters 
+
