2614c2311471eb803ac355bab62e9689a55dca57
Geetanjali Joshi <joshigeetanjali369@gmail.com>
2025-08-18 05:11:45 +0530

    logical operaters

---
+++ logic.js
@@ -0,0 +1,35 @@
+// AND LOGICAL OPERATER (&&)
+
+let marks = 90;
+if(marks>=33 && marks<=91){
+    console.log("all studants pass");
+}
+else{
+    console.log("some studants fail");
+}
+
+
+// AND LOGICAL OPERATER (||)
+
+
+// let marks = 90;
+// if(marks>=33 || marks<=91){
+//     console.log("all studants pass");
+// }
+// else{
+//     console.log("some studants fail");
+// }
+
+
+
+
+// NOT LOGICAL OPRATER (!)
+
+
+// let marks = 90;
+// if(!marks>=33){
+//     console.log("all studants pass");
+// }
+// else{
+//     console.log("some studants fail");
+// }
