c936cfec110a2f796e71b0bd3d3d2f2c78de72fb
Geetanjali Joshi <joshigeetanjali369@gmail.com>
2025-08-18 05:37:52 +0530

    problem statment 1

---
+++ pStatment1.js
@@ -0,0 +1,13 @@
+let str = "Application";
+console.log(str);
+
+if(str[0]==="A" ){
+    console.log("good string");
+ if(str.length==3){
+    console.log("str is a good");
+ }
+ else
+ {
+   console.log("length is not match");
+ }
+}
