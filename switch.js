f96cffa8c58d1432ebcdc0c89045cbe43cc63e04
Geetanjali Joshi <joshigeetanjali369@gmail.com>
2025-08-19 22:41:36 +0530

    switch statement

---
+++ switch.js
@@ -0,0 +1,15 @@
+let color="red";
+switch(color){
+    case "red":
+        console.log("stop");
+        break;
+    case "green":
+        console.log("slow down");
+        break;
+    case "yellow":
+        console.log("go");
+        break;   
+        default:
+            console.log("light is broken")   ;
+
+}
