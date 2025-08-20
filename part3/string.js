d742ca7c27e32c186d45f260dbb44a8d61e684e0
Geetanjali Joshi <joshigeetanjali369@gmail.com>
2025-08-20 23:02:43 +0530

    add string method

---
+++ part3/string.js
@@ -0,0 +1,26 @@
+let msg = " hello  ";
+msg.trim();
+console.log(msg);
+
+// ushing trim methad (trim the last and first extra space)
+
+let a = prompt("enter your passward");
+a.trim();
+console.log(a)
+// using lower case and upper case 
+
+let p = "my name is khushi";
+p.toUpperCase();
+console.log(p.toUpperCase());
+
+let k = "I AM BCA AI & DS STUDENT";
+k.toLowerCase();
+console.log(k.toLowerCase());
+
+
+
+// string methods with arguments
+
+
+let msg2= "love";
+console.log(msg2);
