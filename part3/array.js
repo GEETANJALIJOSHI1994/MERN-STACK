38c2f936488fa875c462662a9392bc688c3d18ad
Geetanjali Joshi <joshigeetanjali369@gmail.com>
2025-08-22 04:00:20 +0530

    array (data strucure)

---
+++ part3/array.js
@@ -0,0 +1,22 @@
+// create array
+
+let student = ["ram","kamal","sheetal"]
+console.log(student);
+
+// visualizing Array
+
+let num = [2,4,5,6];
+console.log(num[0]);
+console.log(num[1]);
+console.log(num[2]);
+console.log(num[3]);
+console.log(num.length);
+console.log(typeof num);
+
+// creating array diffrent fome
+
+let number = [1,2,3,4,5];
+let name = ["ram","raghaw","rohit"];
+let info = ["sheetal",4,7,9.0];
+console.log(number,name,info);
+
