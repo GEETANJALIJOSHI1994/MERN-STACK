b78a7b128d98a4422165b13ba14893d9c8311e64
Geetanjali Joshi <joshigeetanjali369@gmail.com>
2025-08-23 00:41:46 +0530

    pStatement5

---
+++ part3/p.Statements5.js
@@ -0,0 +1,10 @@
+let months = ["january","july","march",'august'];
+console.log(months);
+console.log(months.shift());
+console.log(months);
+console.log(months.shift());
+console.log(months);
+console.log(months.unshift("june"));
+console.log(months);
+console.log(months.unshift("july"));
+console.log(months);
