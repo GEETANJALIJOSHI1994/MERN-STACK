0e523117eeb6616cec3c8bfcfa70eb4f78422113
Geetanjali Joshi <joshigeetanjali369@gmail.com>
2025-08-25 20:06:45 +0530

    array

---
+++ part3/p.Statement6.js
@@ -0,0 +1,8 @@
+ let months = ["january","july","march","augest"];
+ console.log(months);
+ console.log(months.splice(0,2,"july","june"));
+ console.log(months);
+
+
+//  return the index of the "js" from the given array.if it was reverse
+
