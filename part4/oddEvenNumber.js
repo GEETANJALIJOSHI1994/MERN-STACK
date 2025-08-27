a7303709d892b9250f8ac358a85d2b27596d0c4b
Geetanjali Joshi <joshigeetanjali369@gmail.com>
2025-08-27 09:13:44 +0530

    odd even

---
+++ part4/oddEvenNumber.js
@@ -0,0 +1,22 @@
+let i = 15;
+for(i=1;i<=15;i=i+2){
+    console.log(i);
+}
+
+// backwords odd number
+
+let j = 100;
+for(j=100;j>=0;j=j-3){
+    console.log(j);
+}
+
+
+// print even number (2 to 10)
+
+let k = 10;
+for(k=2;k<=10;k=k+2){
+    console.log(k);
+}
+
+
+// backword even number
