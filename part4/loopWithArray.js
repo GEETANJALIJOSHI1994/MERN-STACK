b8b9e1b6412111827df4dbd6d0c0fd129cfedb40
Geetanjali Joshi <joshigeetanjali369@gmail.com>
2025-08-29 12:02:18 +0530

    nested loop

--- part4/loopWithArray.js
+++ part4/loopWithArray.js
@@ -2,3 +2,14 @@ let fruits = ["mango","banana","apple","litchi","orange"]
 for(let i =0;i<fruits.length;i++){
     console.log(i,fruits[i]);
 }
+
+
+// nested loop
+
+let heroes=[["ironman","spiderman","thor"] , ["superman","wonder woman","flash"]];
+for(let i =0;i<heroes.length;i++){
+    console.log(i,heroes[i],heroes[i].length);
+    for(let j=0;j<heroes[i].length;j++){
+        console.log(`j=${j}, ${heroes[i][j]}`);
+    }
+}
