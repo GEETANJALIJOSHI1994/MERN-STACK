1a55d8e7a1ccb4b354a387cf5d64cc17df440a0b
Geetanjali Joshi <joshigeetanjali369@gmail.com>
2025-09-06 20:52:53 +0530

    literals

---
+++ part5/thread.js
@@ -0,0 +1,12 @@
+let a1 = "radha";
+console.log(a1);
+
+let twitter ={
+    username: "khushi",
+    content: "terocard",
+    like:100000,
+    reposts: 5,
+    tag : ["@apna college","@graphic"]
+};
+let a = twitter;
+console.log(a);
