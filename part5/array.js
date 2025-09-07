0c462d4ebdafc9549f82b17c7a2e6f50c53ed6c5
Geetanjali Joshi <joshigeetanjali369@gmail.com>
2025-09-07 11:42:09 +0530

    array of objects

---
+++ part5/array.js
@@ -0,0 +1,22 @@
+const student = [
+     {
+            name : "rohit",
+            age : 23,
+            marks : 89,
+     },
+      {
+            name : "mohit",
+            age : 26,
+            marks : 99,
+     },
+      {
+            name : "sobit",
+            age : 25,
+            marks : 88,
+     }
+    ]; 
+console.log(student); 
+student[1].age = 27;
+console.log(student); 
+console.log(student[2].marks); 
+console.log(student); 
