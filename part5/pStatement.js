c3abbd68448ea4fd445997ace30f29baf2f17fa7
Geetanjali Joshi <joshigeetanjali369@gmail.com>
2025-09-07 20:40:46 +0530

    object

---
+++ part5/pStatement.js
@@ -0,0 +1,11 @@
+let Random = Math.random();
+console.log(Random);
+console.log("---------------------");
+
+let num = Math.floor(Math.random()*100 + 1);
+console.log (num);
+
+console.log("--------------");
+
+let num2  = Math.floor(Math.random()*5 +1);
+console.log(num2);
