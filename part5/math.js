f48e0114f5e0711624ecaee7667cc7801e1ed4b7
Geetanjali Joshi <joshigeetanjali369@gmail.com>
2025-09-07 14:16:40 +0530

    math in object

---
+++ part5/math.js
@@ -0,0 +1,20 @@
+let PI = Math.PI;
+console.log(PI);
+
+let E = Math.E;
+console.log(E);
+
+let ABS = Math.abs(6);
+console.log(ABS);
+
+let POWER = Math.pow(2,3);
+console.log(POWER);
+
+let FLOOR = Math.floor(23.6666);
+console.log(FLOOR);
+
+let CEIL = Math.ceil(18.6);
+console.log(CEIL);
+
+let RANDOM = Math.random();
+console.log(RANDOM);
