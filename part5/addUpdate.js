1076f55120c4182193bb629ef35885e5cc41bb59
Geetanjali Joshi <joshigeetanjali369@gmail.com>
2025-09-07 10:16:49 +0530

    object update

---
+++ part5/addUpdate.js
@@ -0,0 +1,17 @@
+const student = {
+    name : "khushi",
+    age : 30,
+    marks : 98.9,
+    city :"haldwani"
+
+};
+// const student2 = student;
+// console.log(student2);
+
+console.log(student); 
+student.city = "dehradoon";
+console.log(student);
+student.gender = "female";
+console.log(student);
+console.log(delete student.marks);
+console.log(student);
