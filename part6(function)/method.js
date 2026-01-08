361cec1073f0007c77c330bc8853321df5c45e44
Geetanjali Joshi <joshigeetanjali369@gmail.com>
2026-01-08 16:49:15 +0530

    Function

---
+++ part6(function)/method.js
@@ -0,0 +1,12 @@
+const calculater ={
+    add: function(a,b){
+        return a+b;
+    },
+    sub: function(a,b){
+        return a-b;
+    },
+    mul: function(a,b){
+        return a*b;
+    }
+};
+console.log(calculater.add(34,67));
