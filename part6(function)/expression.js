67621bfbd9af56eca1b2b33937efea32b56eb18b
Geetanjali Joshi <joshigeetanjali369@gmail.com>
2026-01-08 16:39:23 +0530

    FUNCTION

--- part6(function)/expression.js
+++ part6(function)/expression.js
@@ -21,3 +21,22 @@ function multipalGreet(func,count){
     console.log('good morning');
  }
  multipalGreet(greet,9);
+
+
+
+ //return a function from a function
+ function oddEvenFactory(request){
+    if(request=='odd'){
+let odd = function(n){
+    console.log(!(n%2==0));
+}
+return odd;
+    } else if (request=='even'){
+        let even = function(n){
+            console.log(n%2==0);
+        }
+        return even;
+    }
+ }
+
+ let request = 'odd';
