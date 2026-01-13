c53fac0ece68c99d8818d91de4b077fed30f74f6
Geetanjali Joshi <joshigeetanjali369@gmail.com>
2026-01-13 15:35:37 +0530

    this in arrow function

--- part6(function)/this.js
+++ part6(function)/this.js
@@ -7,10 +7,44 @@ const students={
     getAvg(){
         let avg=(this.eng+this.maths+this.phy)/3;
         console.log(`${this.name} got average marks = ${avg}`);
-    }
-}
+    },
+};
 
 
 
 //this with arrow function
 
+const students2={
+    name1:'shiyam',
+    age1:23,
+    eng1:99,
+    math:98,
+    prop: this,
+    getName(){
+        console.log(this);
+        return this.name;
+    },
+     getMath:()=>{
+        console.log(this);
+        return this.math;
+    },
+};
+
+
+
+const student={
+    nm: 'riya',
+    mks: 90,
+    getInfo1:function(){
+        setTimeout(()=>{
+            console.log(this);
+        },2000)
+            
+    },
+    getInfo2:function(){
+        setTimeout(function(){
+            console.log(this);
+        },2000)
+            
+    },
+};
