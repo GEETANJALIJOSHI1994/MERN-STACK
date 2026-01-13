fd7cc917d8a4707a86c14146d8f32cb2fe725930
Geetanjali Joshi <joshigeetanjali369@gmail.com>
2026-01-13 14:41:38 +0530

    Timeout

--- part6(function)/arrowFunction.js
+++ part6(function)/arrowFunction.js
@@ -1,11 +1,18 @@
-const sum=(a,b)=>{
+const sum1 = (a,b) => {
     console.log(a+b);
-}
+};
 
 const cube=(n)=>{
     return n*n*n;
-}
+};
 
 const power =(a,b)=>{
     return a**b;
-}
+};
+
+
+//IMPLICIT RETURN
+
+const mul=(a,b)=>{
+    a*b;
+};
