4102bd1b0f01d492191f0802e8d3177b918d5b0a
Geetanjali Joshi <joshigeetanjali369@gmail.com>
2026-01-18 13:14:14 +0530

    add destructar

--- part 7 array methods/Rest.js
+++ part 7 array methods/Rest.js
@@ -2,3 +2,12 @@ function sum(...args){
     return args.reduce((add,el)=>
         add+el);
 }
+
+
+//DESTRUCTRING 
+ const namess=['ram','shyam','rohan','mohan'];
+ const[winner,runner,...other]=namess;
+ console.log(winner,runner);
+ console.log(runner);
+ console.log(winner);
+ console.log(other);
