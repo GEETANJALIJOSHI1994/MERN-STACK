6287953e501e28a28cdb7ab4cfaac92f7ee8c6d6
Geetanjali Joshi <joshigeetanjali369@gmail.com>
2026-01-18 09:12:15 +0530

    spred

--- part 7 array methods/Spread.js
+++ part 7 array methods/Spread.js
@@ -2,3 +2,13 @@
 
 let k1=[4,6,7,8,9,9,2,4];
 console.log(...k1);
+
+//with array literals
+
+let chars=[..."apnacollege"];
+console.log(chars);
+
+let odd=[1,3,5,7];
+let even=[2,4,6,8];
+let num=[...odd,...even];
+console.log(num);
