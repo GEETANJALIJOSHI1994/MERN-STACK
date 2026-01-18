dae14f8c7d1b1181541da954b42d4dda137984e7
Geetanjali Joshi <joshigeetanjali369@gmail.com>
2026-01-18 08:13:28 +0530

    maximu value

--- part 7 array methods/reduce.js
+++ part 7 array methods/reduce.js
@@ -5,3 +5,25 @@ const result=a2.reduce((res,el)=>{
    return res+el;
 })
 console.log(result);
+
+
+const a5=[1,2,3,94,5,6,7,78];
+const maximum=a5.reduce((max,el)=>{
+    if(el>max){
+        return el;
+    }else{
+        return max;
+    }
+})
+console.log(maximum);
+
+
+
+const a6=[5,78,69,90,99];
+let max=-1;
+for(let i=0;i<a6.length;i++){
+    if(a6[i]>max){
+        max=a6[i];
+    }
+}
+console.log(max);
