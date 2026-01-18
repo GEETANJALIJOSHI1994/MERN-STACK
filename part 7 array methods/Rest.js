a96777b30b665f2269dbdbc3ed644e7c20795755
Geetanjali Joshi <joshigeetanjali369@gmail.com>
2026-01-18 13:28:14 +0530

    add destructuring in objects

--- part 7 array methods/Rest.js
+++ part 7 array methods/Rest.js
@@ -11,3 +11,21 @@ function sum(...args){
  console.log(runner);
  console.log(winner);
  console.log(other);
+
+
+ //DESTRUCTING (OBJECTS)
+
+
+ const person1={
+    nam:'rohit',
+    age:23,
+    city: 'delhi',
+    subject:['math','english','hindi'],
+    username:'karan@123',
+    password:'7788'
+ };
+ let{username:user,password:secret,city:place}=person1;
+ console.log(user);
+ console.log(secret);
+ console.log(place);
+
