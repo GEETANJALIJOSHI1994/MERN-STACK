684cd9b29280f25b2160dd3f94542afc6e74e2d2
Geetanjali Joshi <joshigeetanjali369@gmail.com>
2026-01-20 17:55:28 +0530

    setting content in object

---
+++ part-8 DOM/contentInObject.js
@@ -0,0 +1,9 @@
+let parag= document.querySelector('p ');
+let a = parag.innerText;
+let a1 = parag.innerContent;
+let a2 = parag.innerHTML;
+
+console.log(parag);
+console.log(a);
+console.log(a1);
+console.log(a2);
