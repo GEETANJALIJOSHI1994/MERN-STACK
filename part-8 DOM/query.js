d625e2d2d411c96a1cc218256e8c3cdbbf227305
Geetanjali Joshi <joshigeetanjali369@gmail.com>
2026-01-20 16:27:17 +0530

    query selector

---
+++ part-8 DOM/query.js
@@ -0,0 +1,17 @@
+let x = document.querySelector('h1');
+console.log(x);
+
+let x1 = document.querySelector('p');
+console.log(x1);
+
+let x2 = document.querySelector('div');
+console.log(x2);
+
+let x3 = document.querySelector('.unlist1');
+console.log(x3);
+
+let x4 = document.querySelector('#mainImg');
+console.log(x4);
+
+let x5 = document.querySelectorAll('h1');
+console.log(x5);
