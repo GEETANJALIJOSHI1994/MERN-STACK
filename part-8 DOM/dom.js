35bdf0519fde281bf8f01068bd5c0c6a9bafede3
Geetanjali Joshi <joshigeetanjali369@gmail.com>
2026-01-20 16:13:38 +0530

    select elements my propertys

---
+++ part-8 DOM/dom.js
@@ -0,0 +1,8 @@
+let imgs=document.getElementById("mainImg");
+console.log(imgs);
+
+let unlist=document.getElementsByClassName("unlist1");
+console.log(unlist);
+
+let para=document.getElementsByTagName("p");
+console.log(para);
