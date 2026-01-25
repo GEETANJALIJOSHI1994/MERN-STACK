ca3901f651e9345f08388392dff41ba71e06b873
Geetanjali Joshi <joshigeetanjali369@gmail.com>
2026-01-25 12:12:38 +0530

    navigation

---
+++ part-8 DOM/navigation.js
@@ -0,0 +1,16 @@
+let list = document.querySelectorAll('li');
+list.parantElement;
+console.log(list);
+
+let unlist1 = document.querySelectorAll('ul');
+unlist1.parantElement;
+console.log(unlist1);
+
+
+
+
+let div2 = document.querySelectorAll('div');
+div2.children;
+let p=div2.children.previousElementSibling;
+console.log(div2); 
+
